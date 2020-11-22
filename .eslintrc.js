a5b02c54a53a630f5431a3579b0939400a70aed2
Emmanuel Neni <emmanuelneni@gmail.com>
2020-11-22 17:47:26 +0200

    base project config setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    jest: true,
   },
   parserOptions: {
     parser: 'babel-eslint',
@@ -16,5 +17,31 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/require-default-prop': 'off',
+    'vue/no-use-v-if-with-v-for': 'off',
+    'no-use-v-if-with-v-for': 'off',
+    'vue/no-v-html': 'off',
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    // 'no-param-reassign': 0,
+    // 'no-return-assign': 0,
+    // 'no-undef': 'warn',
+    // 'linebreak-style': 0,
+  },
+  overrides: [
+    {
+      files: '**/*.test.js',
+      rules: {
+        'node/no-unpublished-require': 0,
+        'node/no-missing-require': 0,
+      },
+    },
+  ],
 }
