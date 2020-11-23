40c23da9a94edb9f74b55cf3f52f9b85cf1c11b9
Emmanuel Neni <emmanuelneni@gmail.com>
2020-11-23 16:04:35 +0200

    cleaning up forms

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -10,42 +10,11 @@
       You have to sign in before accessing to
       <strong>{{ $auth.$state.redirect }}</strong>
     </b-alert>
-    <b-row align-h="center" class="pt-4">
+    <b-row align-h="center">
       <b-col lg="4" md="6">
-        <b-card bg-variant="light">
+        <b-card bg-variant="light" class="p-1 p-sm-3 p-md-4">
           <busy-overlay />
-          <form>
-            <b-form-group label="Email">
-              <b-input ref="email" v-model="email" autofocus placeholder="Email address" />
-            </b-form-group>
-
-            <b-form-group label="Password">
-              <b-input-group>
-                <b-input-group-prepend is-text class="clickable">
-                  <b-icon
-                    :icon="passwordIcon"
-                    font-scale="1.5"
-                    @click="hidePassword = !hidePassword"
-                  >
-                  </b-icon>
-                </b-input-group-prepend>
-                <b-input
-                  v-model="password"
-                  :type="passwordType"
-                  placeholder="password"
-                  required
-                  name="password"
-                />
-              </b-input-group>
-            </b-form-group>
-
-            <div class="text-center">
-              <b-btn variant="primary" block @click="signIn"> sign in </b-btn>
-            </div>
-            <div class="text-center" style="margin-top: 20px">
-              Don't have an account yet? <nuxt-link to="/sign-up">Sign Up</nuxt-link>
-            </div>
-          </form>
+          <AuthForm :sign-up-page="false" />
         </b-card>
       </b-col>
     </b-row>
@@ -67,12 +36,6 @@ export default {
     }
   },
   computed: {
-    passwordType() {
-      return this.hidePassword ? 'password' : 'text'
-    },
-    passwordIcon() {
-      return this.hidePassword ? 'eye-fill' : 'eye-slash-fill'
-    },
     redirect() {
       return this.$route.query.redirect && decodeURIComponent(this.$route.query.redirect)
     },
@@ -80,41 +43,8 @@ export default {
       return Boolean(this.$route.query.callback)
     },
   },
-  methods: {
-    signIn() {
-      this.error = null
-      return this.$auth
-        .loginWith('local', {
-          data: {
-            email: this.email,
-            password: this.password,
-          },
-        })
-        .then((response, append = false) => {
-          this.$store.dispatch('toast/setToast', {
-            title: response.data.message,
-            variant: response.data.type,
-            text: `You have successfully signed in, ${this.$auth.user.userName}`,
-            delay: 5000,
-          })
-        })
-        .catch((error) => {
-          this.error = error
-        })
-    },
-  },
+  methods: {},
 }
 </script>
 
-<style lang="scss" scoped>
-.login-button {
-  border: 0;
-}
-.clickable {
-  cursor: pointer;
-}
-
-a {
-  color: #7625bc;
-}
-</style>
+<style lang="scss" scoped></style>
--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -11,83 +11,11 @@
       You have to be login before accessing to
       <strong>{{ $auth.$state.redirect }}</strong>
     </b-alert>
-    <b-row align-h="center" class="pt-4">
+    <b-row align-h="center">
       <b-col lg="4" md="6">
-        <b-card bg-variant="light">
+        <b-card bg-variant="light" class="p-1 p-sm-3 p-md-4">
           <busy-overlay />
-          <form @keydown.enter="signUp">
-            <b-form-group
-              label="Email"
-              description="We'll never share your email with anyone else."
-            >
-              <b-input
-                ref="email"
-                v-model="email"
-                placeholder="Email address"
-                required
-                type="email"
-                name="email"
-              />
-            </b-form-group>
-
-            <b-form-group label="Username">
-              <b-input
-                ref="userName"
-                v-model="userName"
-                placeholder="Username"
-                required
-                type="text"
-                name="userName"
-              />
-            </b-form-group>
-
-            <b-form-group label="Password">
-              <b-input-group>
-                <b-input-group-prepend is-text class="clickable">
-                  <b-icon
-                    :icon="passwordIcon"
-                    font-scale="1.5"
-                    @click="hidePassword = !hidePassword"
-                  >
-                  </b-icon>
-                </b-input-group-prepend>
-                <b-input
-                  v-model="password"
-                  :type="passwordType"
-                  placeholder="password"
-                  required
-                  name="password"
-                />
-              </b-input-group>
-            </b-form-group>
-
-            <b-form-group label="Confirm password">
-              <b-input-group>
-                <b-input-group-prepend is-text class="clickable">
-                  <b-icon
-                    :icon="passwordIcon"
-                    font-scale="1.5"
-                    @click="hidePassword = !hidePassword"
-                  >
-                  </b-icon>
-                </b-input-group-prepend>
-                <b-input
-                  v-model="repeat_password"
-                  :type="passwordType"
-                  placeholder="Confirm password"
-                  required
-                  name="repeat_password"
-                />
-              </b-input-group>
-            </b-form-group>
-
-            <div class="text-center">
-              <b-btn variant="primary" block @click="signUp"> Sign Up </b-btn>
-            </div>
-            <div class="text-center" style="margin-top: 20px">
-              Already got an account? <nuxt-link to="/sign-in">Sign In</nuxt-link>
-            </div>
-          </form>
+          <AuthForm :sign-up-page="true" />
         </b-card>
       </b-col>
     </b-row>
@@ -95,75 +23,14 @@
 </template>
 
 <script>
-import busyOverlay from '~/components/busy-overlay'
 export default {
   name: 'SignIn',
-  components: { busyOverlay },
   data() {
-    return {
-      userName: '',
-      email: '',
-      password: '',
-      repeat_password: '',
-      error: null,
-      hidePassword: true,
-    }
+    return {}
   },
-  computed: {
-    passwordType() {
-      return this.hidePassword ? 'password' : 'text'
-    },
-    passwordIcon() {
-      return this.hidePassword ? 'eye-fill' : 'eye-slash-fill'
-    },
-  },
-  methods: {
-    async signUp() {
-      try {
-        await this.$axios
-          .post('/api/auth/signup', {
-            userName: this.userName,
-            email: this.email,
-            password: this.password,
-            repeat_password: this.repeat_password,
-          })
-          .then((response, append = false) => {
-            this.$store.dispatch('toast/setToast', {
-              title: 'Success',
-              variant: response.data.type,
-              text: response.data.message,
-              delay: 5000,
-            })
-          })
-        await this.$auth
-          .loginWith('local', {
-            data: {
-              email: this.email,
-              password: this.password,
-            },
-          })
-          .then((response, append = false) => {
-            this.$store.dispatch('toast/setToast', {
-              title: response.data.message,
-              variant: response.data.type,
-              text: `You have successfully sign up and have been signed in, ${this.$auth.user.userName}`,
-              delay: 5000,
-            })
-          })
-      } catch (error) {
-        this.error = error.response.data.error
-      }
-    },
-  },
+  computed: {},
+  methods: {},
 }
 </script>
 
-<style lang="scss" scoped>
-.clickable {
-  cursor: pointer;
-}
-
-a {
-  color: #7625bc;
-}
-</style>
+<style lang="scss" scoped></style>
