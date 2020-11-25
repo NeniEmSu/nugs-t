17a4a5522883ad8643d141a0086e59cc88530c33
Emmanuel Neni <emmanuelneni@gmail.com>
2020-11-25 14:02:58 +0200

    feature: added legal pages

---
+++ pages/faqs.vue
@@ -0,0 +1,21 @@
+<template>
+  <Page :data="page" type="page" />
+</template>
+
+<script>
+import Page from '~/components/ArticleAndPage.vue'
+export default {
+  components: {
+    Page,
+  },
+  async asyncData({ app, store, params }) {
+    const { data } = await app.$axios.get(`/wp/v2/pages?slug=frequently-asked-questions`, {
+      params: {
+        slug: params.page,
+        _embed: true,
+      },
+    })
+    return { page: data[0] }
+  },
+}
+</script>
