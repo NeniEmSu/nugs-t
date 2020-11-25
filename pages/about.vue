68e597f85e849e7085785ba95a8e8b7dca4b0bbc
Emmanuel Neni <emmanuelneni@gmail.com>
2020-11-25 10:20:49 +0200

    feature: adding hero

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <h1>About Us</h1>
+    <TheHero :heading="heading" :sup-title="supTitle" :sub-title="subTitle" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'About',
+  data() {
+    return {
+      heading: 'About Us',
+      supTitle: 'Executives 2020-2021',
+      subTitle:
+        'We are a Democratic, nonpartisan and progressive mass movement comprising of ghanaian students in Ternopol, ukraine with the primary aim of protecting and safeguarding the rights and interests of ghanaian students in ternopol.',
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
