69a610e85c10032cf649f348b2f482f588acc789
Emmanuel Neni <emmanuelneni@gmail.com>
2020-12-06 17:08:44 +0200

    [FIX] styling and reuse func

--- components/busy-overlay.vue
+++ components/busy-overlay.vue
@@ -1,20 +1,37 @@
 <template>
-  <div v-if="$auth.busy" class="overlay">
+  <div v-if="loading" class="overlay">
     <img src="~/assets/svg/loading.svg" alt="Loading..." />
+    <br />
+    <h2>Just a sec...</h2>
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    loading: {
+      default: false,
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
 <style scoped>
 .overlay {
+  z-index: 2;
   position: absolute;
-  background-color: rgba(255, 255, 255, 0.5);
-  color: grey;
+  background-color: rgba(255, 255, 255, 0.8);
+  color: black;
   left: 0;
   right: 0;
   bottom: 0;
   top: 0;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  border-radius: 15px;
 }
 </style>
